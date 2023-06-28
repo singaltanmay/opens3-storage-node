af24cf943aaa26c245e02600980b892dd9c624e1
Tanmay Singal <tanmaysingal@icloud.com>
2023-06-27 20:43:52 -0700

    checkpoint

--- src/main/java/com/tanmaysingal/opens3/nodemetadata/NodeObjectMetadataRepository.java
+++ src/main/java/com/tanmaysingal/opens3/nodemetadata/NodeObjectMetadataRepository.java
@@ -2,9 +2,10 @@ package com.tanmaysingal.opens3.nodemetadata;
 
 import com.tanmaysingal.opens3.models.NodeObjectMetadata;
 import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NodeObjectMetadataRepository extends JpaRepository {
+public interface NodeObjectMetadataRepository extends JpaRepository<NodeObjectMetadata, UUID> {
 
   Optional<NodeObjectMetadata> findByEtag(String etag);
 }
